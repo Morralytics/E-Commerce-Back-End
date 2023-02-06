493dcf6f8801c4e5e3dc5676e19d372c5df6bb9f
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-05 23:31:29 -0800

    Adds a new tag.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,11 +10,6 @@ router.get('/', async (req, res) => {
   };
 });
 
-  // router.get('/:id', (req, res) => {
-  //   // find a single tag by its `id`
-  //   // be sure to include its associated Product data
-  // });
-
   router.get('/:id', async (req, res) => {
     try {
       const tagData = await Tag.findByPk(req.params.id);
@@ -24,8 +19,13 @@ router.get('/', async (req, res) => {
     };
   });
 
-  router.post('/', (req, res) => {
-    // create a new tag
+  router.post('/', async (req, res) => {
+    try {
+      const newTagData = await Tag.create(req.body);
+      res.status(200).json(newTagData);
+    } catch (err) {
+      res.status(200).json(err);
+    };
   });
 
   router.put('/:id', (req, res) => {
